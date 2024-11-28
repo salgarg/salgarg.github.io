17ab8f99e0d0b344bb7e9cc74011d52db33b49dc
Saloni <saloni.garg.00@gmail.com>
2024-11-28 15:01:41 -0600

    added assets to public

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import AboutMe from './components/AboutMe';
+import Photography from './components/Photography';
+import DigitalArt from './components/DigitalArt';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav style={{ padding: '1rem', backgroundColor: '#f5f5f5' }}>
+        <Link to="/" style={{ margin: '0 1rem' }}>About Me</Link>
+        <Link to="/photography" style={{ margin: '0 1rem' }}>Photography</Link>
+        <Link to="/digital-art" style={{ margin: '0 1rem' }}>Digital Art</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<AboutMe />} />
+        <Route path="/photography" element={<Photography />} />
+        <Route path="/digital-art" element={<DigitalArt />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section style={{ padding: '2rem' }}>
+      <h1>About Me</h1>
+      <p>Hi! I'm Saloni Garg! I'm currently a Product Manager at SeatGeek. Welcome to my portfolio!</p>
+    </section>
+  );
+};
+
+export default AboutMe;
