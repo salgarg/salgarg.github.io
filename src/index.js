e6d9bbb69be4b05338897fd293ccff4a8d8e032f
Saloni <saloni.garg.00@gmail.com>
2024-11-28 14:28:36 -0600

    Start fresh

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';  // Add this if you have a CSS file
+import App from './App';  // Import your main App component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
